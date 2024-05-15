14f9ac6683d4cdc76b8976791f5d1851ab95ed24
1105360803 <1105360803@SJ106PC11.fup.edu>
2024-05-15 11:46:05 -0500

    Parking

--- src/Parkingg/Parking.java
+++ src/Parkingg/Parking.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Parkingg;
 
-/**
- *
- * @author 1105360803
- */
+import java.util.ArrayList;
+import java.util.Collections;
+
+
 public class Parking {
+    
+    private ArrayList<String> matriculas;
+    private String nombre;
+
+    public Parking(String nombre,int posicion) {
+        this.nombre = nombre;
+        matriculas = new ArrayList<>(Collections.nCopies(posicion, null));
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void entrada(String matricula,int posicion) throws ParkingException{
+        if(posicion>=matriculas.size() || posicion <0)
+            throw new ParkingException("Posicion Inexistente",matricula);
+        if(matricula==null || matricula.length()<4)
+            throw new ParkingException("Matricula Incorrecta", matricula);
+        if(matriculas.get(posicion)!=null)
+            throw new ParkingException("Posicion ocupada",matricula);
+        if(matriculas.contains(matricula))
+        matriculas.set(posicion,matricula);
+        
+    }
+
+    private static class ParkingException extends Exception {
+
+        public ParkingException(String posicion_Inexistente, String matricula) {
+        }
+    }
+    
+    
     
 }
